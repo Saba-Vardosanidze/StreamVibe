493591870452f5013137fd09414ff6c8cbeda54f
nnnakashidze <nini.nakashidze17@gmail.com>
2025-03-05 02:42:55 +0400

    update

--- js/pages/home.js
+++ js/pages/home.js
@@ -37,3 +37,18 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("showMenu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("showMenu");
+  });
+}
